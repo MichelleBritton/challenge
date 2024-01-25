d7e5adf1c5ccefb888f38ebd8c3f822bd64f3c02
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-01-25 16:29:53 +0000

    commit

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -5,7 +5,7 @@ const Sidebar = () => {
     return  <div className={css.sidebar}>
                 <a href="">My Photos</a>
                 <a href="">My Illustrations</a>
-                <a href="">My Paintins</a>
+                <a href="">My Paintings</a>
             </div>
 }
 
