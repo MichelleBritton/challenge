e9860f2775a604c90c85833c19224249e68776d4
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-02-03 15:50:10 +0000

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,8 +2,10 @@ import css from './App.module.css';
 import Sidebar from "./components/Sidebar";
 // import NavBarSimple from "./components/NavBarSimple";
 import NavBarForm from "./components/NavBarForm";
+import ContentAPI from './components/ContentAPI';
 // import Content from "./components/Content";
-import ContentHooks from './components/ContentHooks';
+// import ContentHooks from './components/ContentHooks';
+
 
 function App() {
   return (
@@ -12,7 +14,8 @@ function App() {
       {/* <NavBarSimple /> */}
       <NavBarForm />
       {/* <Content /> */}
-      <ContentHooks />
+      {/* <ContentHooks /> */}
+      <ContentAPI />
     </div>
   );
 }
