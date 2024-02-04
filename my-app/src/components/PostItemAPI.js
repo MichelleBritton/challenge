3c0493b8e628d6639ede8c040e813e1b3270ae9d
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-02-04 14:52:07 +0000

    commit

--- my-app/src/components/PostItemAPI.js
+++ my-app/src/components/PostItemAPI.js
@@ -2,20 +2,19 @@ import React from "react"
 import css from "./css/PostItem.module.css";
 
 
-function PostItemAPI(props) {
+function PostItem(props) {
     return (
         props.savedPosts.map(post => {
-            // Extra task destructuring 
-            const {title, name, image, description} = post
-            return <div className={css.SearchItem} key={title}>
-                <p>{title}</p>
-                <p>{name}</p>
-                <img src={image} alt="random"/>
-                <p>{description}</p>
+            const {id, type, user, webformatURL, tags} = post
+            return <div className={css.SearchItem} key={id}>
+                <p>{type}</p>
+                <p>{user}</p>
+                <img src={webformatURL} alt="random"/>
+                <p>{tags}</p>
             </div>
             }
         )
     )
 }
 
-export default PostItemAPI
+export default PostItem
